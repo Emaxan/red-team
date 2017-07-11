7e9d7e7bebd6f46f8562c85439fe3c7fcf7e3ae8
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-12 00:09:11 +0300

    Add User model

---
+++ iTechArtSurvay/Server/iTechArtSurvay.Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iTechArtSurvay.Domain {
+	public class User {
+		public int Id { get; set; }
+
+		[Required]
+		[MinLength(3, ErrorMessage = "Name should be at least 3 characters")]
+		public string Name { get; set; }
+
+		[Required]
+		[EmailAddress(ErrorMessage = "Invalid email")]
+		public string Email { get; set; }
+
+		[Required]
+		[MinLength(8, ErrorMessage = "Password should be at least 8 characters")]
+		public string Password { get; set; }
+	}
+}
