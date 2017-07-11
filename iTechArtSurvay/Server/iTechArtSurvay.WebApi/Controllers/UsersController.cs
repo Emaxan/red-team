2b61ac0fee668492286ea646a5f1e7135ad453b4
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-12 00:09:38 +0300

    Add CRUD structure of Users controller

---
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Results;
+using iTechArtSurvay.Domain;
+
+namespace iTechArtSurvay.WebApi.Controllers {
+	public class UsersController : ApiController {
+		//private UserContext db = new UserContext();
+
+		public IHttpActionResult GetUsers() {
+			return new NotFoundResult(Request);
+		}
+
+		public IHttpActionResult GetUser(int id) {
+			return new NotFoundResult(Request);
+		}
+
+		[ResponseType(typeof(void))]
+		public IHttpActionResult PutUser(int id, User user) {
+			return new NotFoundResult(Request);
+		}
+
+		[ResponseType(typeof(User))]
+		public IHttpActionResult PostUser(User user) {
+			return new NotFoundResult(Request);
+		}
+
+		[ResponseType(typeof(User))]
+		public IHttpActionResult DeleteUser(int id) {
+			return new NotFoundResult(Request);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			//if (disposing) db.Dispose();
+			base.Dispose(disposing);
+		}
+
+		private bool UserExist(int id) {
+			return false;
+		}
+	}
+}
