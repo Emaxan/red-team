8eca9e3cb9bafb50d3140509a15de7b67c6a403c
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-12 16:53:47 +0300

    Change constructor and action-methods (see more)
    - Change constructor according to dependency injection container (now IRepository<User> instantiates automatically by DIC
    - Refactor names of action-methods

--- iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
@@ -1,58 +1,62 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
 using System.Web.Http.Description;
-using iTechArtSurvay.Data.Infrastructure;
-using iTechArtSurvay.Data.Repositories;
 using iTechArtSurvay.Domain.Models;
+using iTechArtSurvay.Infrastructure.Repositores;
 
 namespace iTechArtSurvay.WebApi.Controllers
 {
     public class UsersController : ApiController
     {
-        private readonly IRepository<User> _users = new UserRepository();
+        private readonly IRepository<User> usersRepository;
+
+        public UsersController(IRepository<User> usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
 
         // GET api/Users
         [HttpGet]
         public IEnumerable<User> GetUsers()
         {
-            return _users.GetAll();
+            return usersRepository.GetAll();
         }
 
         // GET api/Users/5
         [HttpGet]
         public IHttpActionResult GetUser(int id)
         {
-            var user = _users.Get(id);
-            if ( user == null ) return BadRequest("Wrong Id");
+            var user = usersRepository.Get(id);
+            if (user == null) return BadRequest("Wrong Id");
             return Ok(user);
         }
 
         // PUT api/Users/5
         [HttpPut]
-        [ResponseType(typeof( void ))]
+        [ResponseType(typeof(void))]
         public IHttpActionResult EditUser(int id, User user)
         {
-            _users.Update(user);
+            usersRepository.Update(user);
             return Ok(user);
         }
 
         // POST api/Users
         [HttpPost]
-        [ResponseType(typeof( User ))]
+        [ResponseType(typeof(User))]
         public IHttpActionResult AddUser(User user)
         {
-            _users.Add(user);
+            usersRepository.Add(user);
             return Ok(user);
         }
 
         // DELETE api/Users/5
         [HttpDelete]
-        [ResponseType(typeof( User ))]
+        [ResponseType(typeof(User))]
         public IHttpActionResult RemoveUser(int id)
         {
-            var user = _users.Get(id);
-            if ( user == null ) return BadRequest("Wrong Id");
-            _users.Delete(user);
+            var user = usersRepository.Get(id);
+            if (user == null) return BadRequest("Wrong Id");
+            usersRepository.Delete(user);
             return Ok(user);
         }
 
