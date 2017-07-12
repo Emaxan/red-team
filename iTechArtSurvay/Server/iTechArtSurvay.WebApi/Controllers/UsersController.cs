1f01bedfb58ef44bb4df1681755972b080c83e1c
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-12 16:00:32 +0300

    Update UserController
    Added controller's method implementations.

--- iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
@@ -1,57 +1,65 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 using System.Web.Http.Description;
-using System.Web.Http.Results;
 using iTechArtSurvay.Data.Infrastructure;
 using iTechArtSurvay.Data.Repositories;
 using iTechArtSurvay.Domain.Models;
 
-namespace iTechArtSurvay.WebApi.Controllers {
-    public class UsersController : ApiController {
-        //private UserContext db = new UserContext();
+namespace iTechArtSurvay.WebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private readonly IRepository<User> _users = new UserRepository();
 
         // GET api/Users
         [HttpGet]
-        public IHttpActionResult GetUsers() {
-            IRepository<User> users = new UserRepository();
-            users.Add(new User {
-                                   Name = "Admin",
-                                   Email = "admin@admin.admin",
-                                   Password = "admin"
-                               });
-
-            return new NotFoundResult(Request);
+        public IEnumerable<User> GetUsers()
+        {
+            return _users.GetAll();
         }
 
         // GET api/Users/5
-        public IHttpActionResult GetUser(int id) {
-            return new NotFoundResult(Request);
+        [HttpGet]
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = _users.Get(id);
+            if ( user == null ) return BadRequest("Wrong Id");
+            return Ok(user);
         }
 
         // PUT api/Users/5
+        [HttpPut]
         [ResponseType(typeof( void ))]
-        public IHttpActionResult PutUser(int id, User user) {
-            return new NotFoundResult(Request);
+        public IHttpActionResult EditUser(int id, User user)
+        {
+            _users.Update(user);
+            return Ok(user);
         }
 
         // POST api/Users
+        [HttpPost]
         [ResponseType(typeof( User ))]
-        public IHttpActionResult PostUser(User user) {
-            return new NotFoundResult(Request);
+        public IHttpActionResult AddUser(User user)
+        {
+            _users.Add(user);
+            return Ok(user);
         }
 
         // DELETE api/Users/5
+        [HttpDelete]
         [ResponseType(typeof( User ))]
-        public IHttpActionResult DeleteUser(int id) {
-            return new NotFoundResult(Request);
+        public IHttpActionResult RemoveUser(int id)
+        {
+            var user = _users.Get(id);
+            if ( user == null ) return BadRequest("Wrong Id");
+            _users.Delete(user);
+            return Ok(user);
         }
 
-        protected override void Dispose(bool disposing) {
+        protected override void Dispose(bool disposing)
+        {
             //if (disposing) db.Dispose();
             base.Dispose(disposing);
-        }
-
-        private bool UserExist(int id) {
-            return false; // return db.Users.Count(user => user.Id == id);
         }
     }
 }
