68234eb8e15ce5f2267c43fa65aa13c424d81420
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-12 18:00:39 +0300

    Rename IRepository to IUserRepository and chage it's location
    - Now Repository interfaces will be located in "iTechArtSurvey.Infrastructre.Repositories.Abstract" folder
    - Now Repository implemenations will be located in "iTechArtSurvey.Infrastructre.Repositories.Concrete" folder

---
+++ iTechArtSurvay/Server/iTechArtSurvay.Infrastructure/Repositores/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using iTechArtSurvay.Domain.Models;
+
+namespace iTechArtSurvay.Infrastructure.Repositores.Abstract
+{
+    public interface IUserRepository
+    {
+        void CreateUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(User user);
+        User GetUser(int id);
+        IEnumerable<User> GetUsers();
+    }
+}
---
+++ iTechArtSurvay/Server/iTechArtSurvay.Infrastructure/Repositores/Concrete/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using iTechArtSurvay.Data;
+using iTechArtSurvay.Domain.Models;
+using iTechArtSurvay.Infrastructure.Repositores.Abstract;
+
+namespace iTechArtSurvay.Infrastructure.Repositores.Concrete
+{
+    public class UserRepository : IUserRepository
+    {
+        private ITechArtSurvayContext context = new ITechArtSurvayContext();
+
+        public void CreateUser(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            context.Users.Attach(user);
+            context.Entry(user).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void DeleteUser(User user)
+        {
+            context.Users.Remove(user);
+            context.SaveChanges();
+        }
+
+        public User GetUser(int id)
+        {
+            return context.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return context.Users;
+        }
+    }
+}
