b88b5c470360459fe8d5ff8bf447e98e7d43a61b
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-12 13:09:56 +0300

    Add comments to code

--- iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Results;
 using iTechArtSurvay.Data.Infrastructure;
@@ -7,49 +6,52 @@ using iTechArtSurvay.Data.Repositories;
 using iTechArtSurvay.Domain.Models;
 
 namespace iTechArtSurvay.WebApi.Controllers {
-	public class UsersController : ApiController {
-		//private UserContext db = new UserContext();
+    public class UsersController : ApiController {
+        //private UserContext db = new UserContext();
 
+        // GET api/Users
         [HttpGet]
-		public IHttpActionResult GetUsers() {
+        public IHttpActionResult GetUsers() {
             IRepository<User> users = new UserRepository();
-            users.Add(new User()
-            {
-                Name = "Admin",
-                Email = "admin@admin.admin",
-                Password = "admin"
-            });
+            users.Add(new User {
+                                   Name = "Admin",
+                                   Email = "admin@admin.admin",
+                                   Password = "admin"
+                               });
 
             return new NotFoundResult(Request);
-		}
+        }
 
-		public IHttpActionResult GetUser(int id) {
-			return new NotFoundResult(Request);
-		}
+        // GET api/Users/5
+        public IHttpActionResult GetUser(int id) {
+            return new NotFoundResult(Request);
+        }
 
-		[ResponseType(typeof(void))]
-		public IHttpActionResult PutUser(int id, User user) {
-			return new NotFoundResult(Request);
-		}
+        // PUT api/Users/5
+        [ResponseType(typeof( void ))]
+        public IHttpActionResult PutUser(int id, User user) {
+            return new NotFoundResult(Request);
+        }
 
-		[ResponseType(typeof(User))]
-		public IHttpActionResult PostUser(User user) {
-			return new NotFoundResult(Request);
-		}
+        // POST api/Users
+        [ResponseType(typeof( User ))]
+        public IHttpActionResult PostUser(User user) {
+            return new NotFoundResult(Request);
+        }
 
-		[ResponseType(typeof(User))]
-		public IHttpActionResult DeleteUser(int id) {
-			return new NotFoundResult(Request);
-		}
+        // DELETE api/Users/5
+        [ResponseType(typeof( User ))]
+        public IHttpActionResult DeleteUser(int id) {
+            return new NotFoundResult(Request);
+        }
 
-		protected override void Dispose(bool disposing)
-		{
-			//if (disposing) db.Dispose();
-			base.Dispose(disposing);
-		}
+        protected override void Dispose(bool disposing) {
+            //if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
 
-		private bool UserExist(int id) {
-			return false;
-		}
-	}
+        private bool UserExist(int id) {
+            return false; // return db.Users.Count(user => user.Id == id);
+        }
+    }
 }
