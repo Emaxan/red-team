a0dc9cef7f2fc710db18005ece5dbd70c1a0b0d1
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-12 16:59:29 +0300

    Add unit tests

---
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using iTechArtSurvay.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iTechArtSurvay.WebApi.Controllers.Tests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        [TestMethod]
+        public void GetUserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void AddUserTest()
+        {
+            //arrange
+
+            //act
+            var control = new UsersController();
+            control.AddUser(new User {Email = "email", Id = 1, Name = "name", Password = "password"});
+            //accert
+            Assert.AreEqual(true, true);
+        }
+    }
+}
---
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("iTechArtSurvay.WebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("iTechArtSurvay.WebApi.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fce83b14-960f-4b30-aaf7-4db4984f2ed9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
