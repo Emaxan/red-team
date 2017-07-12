f614e9c75b8f7f132b80b6e0a616fe8d6a33f98d
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-12 16:32:42 +0300

    Add dependency injection container (ninject)

---
+++ iTechArtSurvay/Server/iTechArtSurvay.Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace iTechArtSurvay.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IKernel Kernel
+        {
+            get { return _kernel; }
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ iTechArtSurvay/Server/iTechArtSurvay.WebApi/App_Start/NinjectConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using iTechArtSurvay.Domain.Models;
+using iTechArtSurvay.Infrastructure.Repositores;
+using iTechArtSurvay.Infrastructure.Repositores.Implemenations;
+using Ninject;
+
+namespace iTechArtSurvay.WebApi.App_Start
+{
+    public class NinjectConfigurator
+    {
+        public void Configure(IKernel _kernel)
+        {
+            AddBindings(_kernel);
+        }
+
+        private void AddBindings(IKernel kernel)
+        {
+            kernel.Bind<IRepository<User>>().To<UserRepository>();
+        }
+    }
+}
