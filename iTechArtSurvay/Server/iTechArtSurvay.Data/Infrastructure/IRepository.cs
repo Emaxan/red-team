50abc0505087830c6d41df1911e330836d3f1241
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-12 12:17:13 +0300

    Implement initial version of Repository-pattern
    - IRepository: main repository-interface
    - UserRepository: implementation of IRepository

---
+++ iTechArtSurvay/Server/iTechArtSurvay.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtSurvay.Data.Infrastructure
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ iTechArtSurvay/Server/iTechArtSurvay.Data/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTechArtSurvay.Data.Infrastructure;
+using iTechArtSurvay.Domain.Models;
+
+namespace iTechArtSurvay.Data.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private ITechArtSurvayContext context = new ITechArtSurvayContext();
+
+        public void Add(User entity)
+        {
+            context.Users.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return context.Users;
+        }
+    }
+}
