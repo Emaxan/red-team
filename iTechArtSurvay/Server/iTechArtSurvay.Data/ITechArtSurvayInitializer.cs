63fa1d3160bf5ee356ea1b4101c8d3a5162b2cc4
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-12 16:36:05 +0300

    Create DB-initializer (DropCreateDatabaseAlways)
    It adds few record to database

---
+++ iTechArtSurvay/Server/iTechArtSurvay.Data/ITechArtSurvayInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using iTechArtSurvay.Domain.Models;
+
+namespace iTechArtSurvay.Data
+{
+    public class ITechArtSurvayInitializer : DropCreateDatabaseAlways<ITechArtSurvayContext>
+    {
+        protected override void Seed(ITechArtSurvayContext context)
+        {
+            context.Users.Add(new User() { Name = "Admin", Email = "admin@admin.admin", Password = "admin" });
+            context.Users.Add(new User() { Name = "User", Email = "user@user.user", Password = "user" });
+
+            base.Seed(context);
+        }
+    }
+}
