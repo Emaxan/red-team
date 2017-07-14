592ac592e31487569f0afbbc1179ae2cc39d1802
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-14 15:35:27 +0300

    Remove useless comments from User.cs file

--- iTechArtSurvay/Server/RedTeam.iTechArtSurvay.DomainModel/Entities/User.cs
+++ iTechArtSurvay/Server/RedTeam.iTechArtSurvay.DomainModel/Entities/User.cs
@@ -10,24 +10,12 @@ namespace RedTeam.iTechArtSurvay.DomainModel.Entities
         public int Id { get; set; }
 
         [Required]
-        //[DataType(DataType.Text, ErrorMessage = "Wrong data type")]
-        //[MinLength(3, ErrorMessage = "Name should be at least 3 characters")]
         public string Name { get; set; }
 
         [Required]
-        //[DataType(DataType.EmailAddress, ErrorMessage = "Wrong data type")]
-        //[EmailAddress(ErrorMessage = "Invalid email")]
         public string Email { get; set; }
 
         [Required]
-        //[DataType(DataType.Password, ErrorMessage = "Wrong data type")]
-        //[MinLength(8, ErrorMessage = "Password should be at least 8 characters")]
         public string Password { get; set; }
-
-        //[Required]
-        //[DataType(DataType.Password, ErrorMessage = "Wrong data type")]
-        //[MinLength(8, ErrorMessage = "Password should be at least 8 characters")]
-        //[Compare("Password", ErrorMessage = "Repeated password should be the same as password")]
-        //public string RepeatPassword { get; set; }
     }
 }
