bf9bd88c1d2bbf821a31fad53ecd402a19c9ed4c
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-07-17 13:57:13 +0300

    Remove unused references

--- TechArtSurvey/Server/RedTeam.TechArtSurvey.DomainModel/Entities/User.cs
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.DomainModel/Entities/User.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using RedTeam.Repositories.Interfaces;
 
 namespace RedTeam.TechArtSurvey.DomainModel.Entities
 {
