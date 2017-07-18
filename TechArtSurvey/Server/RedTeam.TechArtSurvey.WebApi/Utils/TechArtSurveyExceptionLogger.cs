f138545327dd42b525d759783419cd074654490d
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-18 10:28:43 +0300

    Create ExceptionLogger and ExceptionHandler
    - ExceptionLogger should be updated according to using ILog
    - ExceptionHandler should be discussed (response format)

---
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Utils/TechArtSurveyExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http.ExceptionHandling;
+using RedTeam.Logger;
+
+namespace RedTeam.TechArtSurvey.WebApi.Utils
+{
+    public class TechArtSurveyExceptionLogger : ExceptionLogger
+    {
+        private readonly ILog _log;
+
+        public TechArtSurveyExceptionLogger(ILog log)
+        {
+            _log = log;
+        }
+
+        public override void Log(ExceptionLoggerContext context)
+        {
+            string message = context.Exception.Message;
+            // use log
+        }
+    }
+}
