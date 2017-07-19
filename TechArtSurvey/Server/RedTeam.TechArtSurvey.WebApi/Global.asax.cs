f0cf174753b2f61988ee1f7707ddaa66e713ec34
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-19 13:27:16 +0300

    Fix response serialization-error

--- TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Global.asax.cs
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Global.asax.cs
@@ -9,6 +9,12 @@ namespace RedTeam.TechArtSurvey.WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
