cbb4e7375a803bc0f18c409bcb89b89ff195059e
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-07-19 13:24:26 +0300

    Create interface and implementation of service response

---
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.Foundation.Interfaces/ServiceResponses/IServiceResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RedTeam.TechArtSurvey.Foundation.Interfaces.ServiceResponses
+{
+    public interface IServiceResponse
+    {
+        ServiceResponseCodes Code { get; set; }
+        object Content { get; set; }
+    }
+}
---
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.Foundation/Services/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿using RedTeam.TechArtSurvey.Foundation.Interfaces.ServiceResponses;
+
+namespace RedTeam.TechArtSurvey.Foundation.Services
+{
+    public class ServiceResponse : IServiceResponse
+    {
+        public ServiceResponseCodes Code { get; set; }
+        public object Content { get; set; }
+    }
+}
