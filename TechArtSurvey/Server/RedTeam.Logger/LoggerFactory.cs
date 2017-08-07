9a31fca1d6797253f0ae92024eaebbe42133a5ca
Mikhail Skirukha <mov1841@gmail.com>
2017-08-07 15:02:36 +0300

    Remove system.web references from solution

--- TechArtSurvey/Server/RedTeam.Logger/LoggerFactory.cs
+++ TechArtSurvey/Server/RedTeam.Logger/LoggerFactory.cs
@@ -1,17 +1,14 @@
 ﻿using RedTeam.Logger.Interfaces;
 using System;
 using System.Reflection;
-using System.Web.Compilation;
 
 namespace RedTeam.Logger
 {
     public static class LoggerFactory
     {
-        private static readonly Assembly WebApi = BuildManager.GetGlobalAsaxType().BaseType.Assembly;
-
         public static ILog GetLogger(Type type)
         {
-            var log = log4net.LogManager.GetLogger(WebApi, type);
+            var log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
             return new Log(log);
         }
     }
