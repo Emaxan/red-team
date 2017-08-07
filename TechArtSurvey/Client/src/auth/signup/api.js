9d49b4f9cc5a42b47c8ffef96708dd964869f966
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-08-07 10:12:06 +0300

    Update signup request for new version of server

--- TechArtSurvey/Client/src/auth/signup/api.js
+++ TechArtSurvey/Client/src/auth/signup/api.js
@@ -1,9 +1,13 @@
 import { API_URL } from '../../app/config';
 
-export const signup = (signupData) => fetch(`${API_URL}/users`, {
+export const signup = (signupData) => fetch(`${API_URL}/account/signup`, {
   method : 'POST',
   headers : {
     'Content-type' : 'application/json',
   },
-  body : JSON.stringify(signupData),
+  body : JSON.stringify({
+    username : signupData.name,
+    email : signupData.email,
+    password : signupData.password,
+  }),
 });
