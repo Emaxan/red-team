9c9d4b9f93a28a5a2eb0975dc06eee4b0b4646b5
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-08-07 20:48:49 +0300

    Rename id itechart-about-tabs to itechartAboutTabs

--- TechArtSurvey/Client/src/about/components/AboutTabs.jsx
+++ TechArtSurvey/Client/src/about/components/AboutTabs.jsx
@@ -9,7 +9,7 @@ const ABOUT_TABS_DEFAULT_ACTIVE_TAB = 0;
 export class AboutTabs extends Component {
   render() {
     return (
-      <Tabs id="itechart-about-tabs" className="about-tabs" defaultActiveKey={ABOUT_TABS_DEFAULT_ACTIVE_TAB}>
+      <Tabs id="itechartAboutTabs" className="about-tabs" defaultActiveKey={ABOUT_TABS_DEFAULT_ACTIVE_TAB}>
         {
           this.props.tabs.map((item, i) => (
             <Tab eventKey={i} key={i} title={item.text} mountOnEnter unmountOnExit className="about-tabs__item clearfix">{item.content}</Tab>
