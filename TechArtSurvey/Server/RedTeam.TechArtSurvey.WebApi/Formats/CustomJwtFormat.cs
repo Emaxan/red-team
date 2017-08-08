5df5966d1b94e634cfe2d4ba442b9cf55054f835
Nerestorellos <mov1841@gmail.com>
2017-08-09 02:38:49 +0300

    Add getting secret key from web.config

--- TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Formats/CustomJwtFormat.cs
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Formats/CustomJwtFormat.cs
@@ -8,18 +8,23 @@ namespace RedTeam.TechArtSurvey.WebApi.Formats
     {
         public string Protect(AuthenticationTicket ticket)
         {
-            var identity = ticket.Identity;
-            var claims = identity.Claims;
-            var handler = new JwtSecurityTokenHandler();
-
+            var claims = ticket.Identity.Claims;
             var issuer = "localhost";
             var audience = "all";
-            var key = Convert.FromBase64String("401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1");
-            var securityKey = new InMemorySymmetricSecurityKey(key);
+            var key = System.Configuration.ConfigurationManager.AppSettings["SecretKey"];
+            var securityKey = new InMemorySymmetricSecurityKey(Convert.FromBase64String(key));
             var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature, SecurityAlgorithms.Sha256Digest, null);
             var now = DateTime.UtcNow;
             var expires = now.AddMinutes(15);
-            var token = new JwtSecurityToken(issuer: issuer, audience: audience, claims: claims, notBefore: now, expires: expires, signingCredentials: signingCredentials);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer, 
+                audience: audience, 
+                claims: claims, 
+                notBefore: now, 
+                expires: expires, 
+                signingCredentials: signingCredentials
+                );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
--- TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Options/JwtOptions.cs
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Options/JwtOptions.cs
@@ -9,12 +9,12 @@ namespace RedTeam.TechArtSurvey.WebApi.Options
         {
             var issuer = "localhost";
             var audience = "all";
-            var key = Convert.FromBase64String("401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1");
+            var key = System.Configuration.ConfigurationManager.AppSettings["SecretKey"];
 
             AllowedAudiences = new[] { audience };
             IssuerSecurityTokenProviders = new[]
             {
-                new SymmetricKeyIssuerSecurityTokenProvider(issuer, key)
+                new SymmetricKeyIssuerSecurityTokenProvider(issuer, Convert.FromBase64String(key))
             };
         }
     }
