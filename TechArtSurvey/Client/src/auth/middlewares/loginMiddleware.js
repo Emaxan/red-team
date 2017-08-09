289395aaeeeada6a3a0a166c9733254126e8e2c8
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-08-09 16:24:05 +0300

    Add jwt-decode module

--- TechArtSurvey/Client/src/auth/middlewares/loginMiddleware.js
+++ TechArtSurvey/Client/src/auth/middlewares/loginMiddleware.js
@@ -1,23 +1,13 @@
-// import jwt from 'jsonwebtoken';
-import utf8 from 'utf8';
+import jwt_decode from 'jwt-decode';
 
 import { LOG_IN_SUCCESS } from '../login/actionTypes';
 import { setUserToken } from '../actions';
 
-function parseJwt (token) {
-  var [ , base64Url] = token.split('.');
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(window.atob(base64));
-}
-
-// console.log(jwt);
-
 export const logInMiddleware = (store) => (next) => (action) => {
   if (action.type === LOG_IN_SUCCESS) {
-    const jwt = parseJwt(action.payload.token);
+    const jwt = jwt_decode(action.payload.token);
     let { unique_name } = jwt;
     const { role } = jwt;
-    unique_name = utf8.decode(unique_name);
     store.dispatch(setUserToken(
       action.payload.token,
       action.payload.refreshToken,
