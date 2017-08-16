10072aa99eebd7b571b34a72b2b0205813235b37
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-16 09:40:37 +0300

    Replace 'people.png' with 'itechart-logo.png'

--- TechArtSurvey/Client/src/about/components/BasicInfo.jsx
+++ TechArtSurvey/Client/src/about/components/BasicInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 
-import Img from './images/people.png';
+import Img from './images/itechart-logo.png';
 
 import './BasicInfo.scss';
 
