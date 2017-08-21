b296ed86b0bd8450daaa8bda41a7fa63b76e64de
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-08-21 22:37:35 +0300

    Remove redundant calls

--- TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Conf/ResponseFilterAttribute.cs
+++ TechArtSurvey/Server/RedTeam.TechArtSurvey.WebApi/Conf/ResponseFilterAttribute.cs
@@ -14,48 +14,50 @@ namespace RedTeam.TechArtSurvey.WebApi.Conf
     {
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
-            if (!actionContext.ModelState.IsValid)
+            if ( actionContext.ModelState.IsValid )
             {
-                List<string> errors = new List<string>();
-                foreach ( var state in actionContext.ModelState )
-                {
-                    errors.AddRange(state.Value.Errors.Select(em => em.ErrorMessage));
-                }
-
-                var json = JsonConvert.SerializeObject(errors);
-                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, json);
+                return;
             }
-            base.OnActionExecuting(actionContext);
+
+            List<string> errors = new List<string>();
+            foreach ( var state in actionContext.ModelState )
+            {
+                errors.AddRange(state.Value.Errors.Select(em => em.ErrorMessage));
+            }
+
+            var json = JsonConvert.SerializeObject(errors);
+            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, json);
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             var objectContent = actionExecutedContext.Response?.Content as ObjectContent;
             var serviceResponse = objectContent?.Value as IServiceResponse;
-            if (serviceResponse != null)
+            if ( serviceResponse == null )
             {
-                HttpResponseMessage response;
-
-                if (serviceResponse.Code == ServiceResponseCode.Ok)
-                {
-                    response = actionExecutedContext.Request.CreateResponse(
-                                                                            HttpStatusCode.OK,
-                                                                            serviceResponse.Content);
-                }
-                else
-                {
-                    List<string> errorMessages = new List<string>
-                                                 {
-                                                     Properties.ResponseMessages.ResourceManager.GetString(serviceResponse.Code.ToString())
-                                                 };
-                    var json = JsonConvert.SerializeObject(errorMessages);
-                    response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.BadRequest, json);
-                    LoggerContext.Logger.Error(errorMessages[0]);
-                }
-
-                actionExecutedContext.Response = response;
+                return;
             }
-            base.OnActionExecuted(actionExecutedContext);
+
+            HttpResponseMessage response;
+
+            if (serviceResponse.Code == ServiceResponseCode.Ok)
+            {
+                response = actionExecutedContext.Request.CreateResponse(
+                                                                        HttpStatusCode.OK,
+                                                                        serviceResponse.Content);
+            }
+            else
+            {
+                List<string> errorMessages = new List<string>
+                                             {
+                                                 Properties.ResponseMessages.ResourceManager.GetString(serviceResponse.Code.ToString())
+                                             };
+                var json = JsonConvert.SerializeObject(errorMessages);
+                response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.BadRequest, json);
+                LoggerContext.Logger.Error(errorMessages[0]);
+            }
+
+            actionExecutedContext.Response = response;
         }
     }
 }
