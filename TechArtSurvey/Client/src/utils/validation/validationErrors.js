f581d2234120e41b8020eeee56b579ad32026743
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-21 17:26:23 +0300

    Replace numerical ids with string values in ValidationErrors

--- TechArtSurvey/Client/src/utils/validation/validationErrors.js
+++ TechArtSurvey/Client/src/utils/validation/validationErrors.js
@@ -1,41 +1,38 @@
-import {
-  USER_NAME_MIN_LENGTH,
-  USER_PASSWORD_MIN_LENGTH,
-} from './constants';
+import * as constants from './constants';
 
 export const validationErrors = {
   userNameRequired : {
-    id : 1,
+    id : constants.USER_NAME_REQUIRED_ERROR_ID,
     message : 'Name is required',
   },
 
   userNameMinLength : {
-    id : 2,
-    message : `Name should have at least ${USER_NAME_MIN_LENGTH} symbols`,
+    id : constants.USER_NAME_MIN_LENGTH_ERROR_ID,
+    message : `Name should have at least ${constants.USER_NAME_MIN_LENGTH} symbols`,
   },
 
   userEmailRequired : {
-    id : 3,
+    id : constants.USER_EMAIL_REQUIRED_ERROR_ID,
     message : 'Email is required',
   },
 
   userEmailIncorrect : {
-    id : 4,
+    id : constants.USER_EMAIL_INCORRECT_ERROR_ID,
     message : 'Email is incorrect',
   },
 
   userPasswordRequired : {
-    id : 5,
+    id : constants.USER_PASSWORD_REQUIRED_ERROR_ID,
     message : 'Password is required',
   },
 
   userPasswordMinLength : {
-    id : 6,
-    message : `Password should have at least ${USER_PASSWORD_MIN_LENGTH} symbols`,
+    id : constants.USER_PASSWORD_MIN_LENGTH,
+    message : `Password should have at least ${constants.USER_PASSWORD_MIN_LENGTH} symbols`,
   },
 
   userConfirmationPasswordMustMatch : {
-    id : 7,
+    id : constants.USER_CONFIRMATION_PASSWORD_MUST_MATCH_ERROR_ID,
     message : 'Passwords must match',
   },
 };
