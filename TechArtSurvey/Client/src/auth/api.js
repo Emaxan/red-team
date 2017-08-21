d56ed7f145ca108b52d2aac730867974e47e8ea6
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-21 16:52:27 +0300

    Fix auth/api according to MimeType.js

--- TechArtSurvey/Client/src/auth/api.js
+++ TechArtSurvey/Client/src/auth/api.js
@@ -1,10 +1,12 @@
 import { AUTH_URL } from '../app/config';
 import { httpUtility } from '../utils/httpUtility';
+import { FORM_URL_ENCODED_DATA } from '../utils/MimeType';
+
 
 export const updateTokens = (refreshToken) => {
   const headers = {
-    'Content-Type' : 'application/x-www-form-urlencoded',
-    'Accept' : 'application/x-www-form-urlencoded',
+    'Content-Type' : FORM_URL_ENCODED_DATA,
+    'Accept' : FORM_URL_ENCODED_DATA,
   };
 
   const body = `grant_type=refresh_token&refresh_token=${refreshToken}`;
