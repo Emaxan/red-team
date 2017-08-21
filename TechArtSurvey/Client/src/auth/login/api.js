f6f85f1d4e55eb464de5f5cf31d14e6d24860885
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-21 15:58:50 +0300

    Create file with MimeType constants in utils folder (MimeType.js)

--- TechArtSurvey/Client/src/auth/login/api.js
+++ TechArtSurvey/Client/src/auth/login/api.js
@@ -2,11 +2,12 @@ import urljoin from 'url-join';
 
 import { AUTH_URL } from '../../app/config';
 import { httpUtility } from '../../utils/httpUtility';
+import { FORM_URL_ENCODED_DATA } from '../../utils/MimeType';
 
 export const login = (loginData) => {
   const headers = {
-    'Content-type' : 'application/x-www-form-urlencoded',
-    Accept : 'application/x-www-form-urlencoded',
+    'Content-type' : FORM_URL_ENCODED_DATA,
+    'Accept' : FORM_URL_ENCODED_DATA,
   };
   const body = `grant_type=password&username=${loginData.email}&password=${loginData.password}`;
 
