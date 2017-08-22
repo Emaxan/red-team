75ca310bbec10dcee7bf47ce5145bb73646d39ec
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-22 14:22:19 +0300

    Complete InvalidTokenMessage component

--- TechArtSurvey/Client/src/auth/resetPassword/components/InvalidTokenMessage.jsx
+++ TechArtSurvey/Client/src/auth/resetPassword/components/InvalidTokenMessage.jsx
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Panel } from 'react-bootstrap';
 
-export class InvalidTokenMessage extends Component {
-  constructor(props) {
-    super(props);
-  }
+import './InvalidTokenMessage.scss';
 
-  render() {
-    return (
-      <div>
-        <h1>Invalid token</h1>
+export const InvalidTokenMessage = () => (
+  <div className="invalid-token-message">
+    <Panel>
+      <h1 className="invalid-token-message__title">Oops! Some problems has occurred...</h1>
+      <div className="invalid-token-message__content">
+        <p>1. Check your internet connection.</p>
+        <p>2. Make sure that you followed correct link.</p>
+        <p>3. If all above is not about you - sorry, but your link lifetime was expired and you should repeat your request.</p>
       </div>
-    );
-  }
-}
+    </Panel>
+  </div>
+);
