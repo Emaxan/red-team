c48bd6e227664a7ebc961081ac594e6e6b199427
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-23 11:16:02 +0300

    Add restrict access for NewSurveyContainer (only for admin users)

--- TechArtSurvey/Client/src/app/components/AppContent.jsx
+++ TechArtSurvey/Client/src/app/components/AppContent.jsx
@@ -23,6 +23,7 @@ import './AppContent.scss';
 const UserList = userIsAuthenticatedRedirect(userIsAdminRedirect(UserListContainer));
 const Login = userIsNotAuthenticatedRedirect(LoginContainer);
 const SignUp = userIsNotAuthenticatedRedirect(SignUpContainer);
+const NewSurvey = userIsAuthenticatedRedirect(userIsAdminRedirect(NewSurveyContainer));
 
 const AppContent = ({ className }) => (
   <div className={'main ' + className}>
@@ -34,7 +35,7 @@ const AppContent = ({ className }) => (
         <Route path={Routes.SignUp.path} component={SignUp}/>
         <Route path={Routes.Login.path} component={Login} />
         <Route path={Routes.About.path} component={AboutContainer} />
-        <Route path={Routes.NewSurvey.path} component={NewSurveyContainer} />
+        <Route path={Routes.NewSurvey.path} component={NewSurvey} />
         <Route component={NotFound}/>
       </Switch>
     </CustomScroll>
