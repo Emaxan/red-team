3c3e212dfa16d2b9d115d472e7d270f0cbc97aec
AlexeyMagdich <Aliaksei.Mahdzich@itechart-group.com>
2017-08-25 11:32:35 +0300

    Fix prop-types error in NotificationPanel

--- TechArtSurvey/Client/src/auth/resetPassword/components/InvalidTokenPanel.jsx
+++ TechArtSurvey/Client/src/auth/resetPassword/components/InvalidTokenPanel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { NotificationPanel } from '../../../components/NotificationPanel';
 
 export const InvalidTokenPanel = () => (
-  <NotificationPanel>
+  <NotificationPanel title="Oops! Some problems has occurred...">
     <p>1. Check your internet connection.</p>
     <p>2. Make sure that you followed correct link.</p>
     <p>3. If all above is not about you - sorry, but your link lifetime was expired and you should repeat your request.</p>
