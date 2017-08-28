23b7888e5eebb5fde718fbb08a4b652f33fd04f4
Maxim Ermoshyn <maksim.yarmoshyn@itechart-group.com>
2017-08-28 15:53:20 +0300

    Add File question

--- TechArtSurvey/Client/src/surveys/components/questions/FileQuestion.jsx
+++ TechArtSurvey/Client/src/surveys/components/questions/FileQuestion.jsx
@@ -1,29 +1,109 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Panel, Col, FormGroup, FormControl } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+
+// import './FileQuestion.scss';
 
 export class FileQuestion extends Component {
   constructor(props) {
     super(props);
+
+    let metaInfo = this.props.question.metaInfo.map(m => m);
+
+    this.state = {
+      number : this.props.question.number,
+      type : this.props.question.type,
+      title : this.props.question.title,
+      isRequired : this.props.question.isRequired,
+      metaInfo : metaInfo,
+    };
   }
 
-  handleOnTextChange = (event) => {
-    var newQuestionState = this.props.question;
-    newQuestionState.text = event.target.value;
+  handleOnTitleChange = (event) => {
+    let title = event.target.value;
+    this.setState({ title : title });
+    let question = {...this.state};
+    question.title = title;
+    this.props.handleOnQuestionUpdate(question);
+  }
 
-    this.props.handleOnQuestionChange(newQuestionState);
+  handleOnFileChange = (event) => {
+    let file = event.target.value;
+    this.setState({ metaInfo : this.fileUpload.files[0].name });
+    let question = {...this.state};
+    question.metaInfo = file;
+    this.props.handleOnQuestionUpdate(question);
+  }
+
+  handleOnOptionChange = (optionId, value) => {
+    let metaInfo = this.state.metaInfo.map(m => m);
+    metaInfo[optionId] = value;
+    this.setState({ metaInfo : metaInfo });
+    let question = {...this.state};
+    question.metaInfo = metaInfo;
+    this.props.handleOnQuestionUpdate(question);
+  }
+
+  handleOnAddOption = () => {
+    let metaInfo = this.state.metaInfo.map(m => m);
+    metaInfo.push('');
+    this.setState({ metaInfo : metaInfo });
+    let question = {...this.state};
+    question.metaInfo = metaInfo;
+    this.props.handleOnQuestionUpdate(question);
   }
 
   render = () => {
     return (
       <Panel>
-        fiiiile
+        <FormGroup>
+          <Col sm={10} smOffset={1}>
+            {
+              this.props.editing ?
+                (
+                  <FormControl
+                    name="title"
+                    type="text"
+                    value={this.state.title}
+                    placeholder="Enter question's title"
+                    onChange={this.handleOnTitleChange}
+                  />
+                ) :
+                (
+                  this.props.question.title
+                )
+            }
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col sm={8} smOffset={1}>
+            <input
+              name="option"
+              type="file"
+              onChange={this.handleOnFileChange}
+              ref={(ref) => this.fileUpload = ref}
+            />
+            <FormControl
+              readOnly
+              name="option"
+              type="text"
+              value={this.state.metaInfo || ''}
+              placeholder="Select file"
+            />
+          </Col>
+        </FormGroup>
       </Panel>
     );
   }
 }
 
 FileQuestion.propTypes = {
-  handleOnQuestionChange : PropTypes.func.isRequired,
-  question : PropTypes.object.isRequired,
+  question: PropTypes.object.isRequired,
+  handleOnQuestionUpdate: PropTypes.func.isRequired,
+  editing : PropTypes.bool,
+};
+
+FileQuestion.defaultProps = {
+  editing : false,
 };
